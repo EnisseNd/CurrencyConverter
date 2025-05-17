f2d57a2c3b538eead8f7b62fba9eacdca0057f19
enissend <enissendikumana07@gmail.com>
2025-05-17 18:20:04 -0500

    fetched exchange rates from api, printed final result fo conversion.

--- solution.py
+++ solution.py
@@ -10,4 +10,12 @@ to_currency = str(
 
 amount = float(
     input("Enter in your monetary amount: ") )
+ 
+ #fetches api, is successful when status is 200
+response = requests.get(
+    f"https://v6.exchangerate-api.com/v6/9ddf48650227d232cc03556a/pair/{from_currency}/{to_currency}/{amount}")
 
+
+print(
+    f"{amount} {from_currency} is equal to {response.json()['conversion_rate']} {to_currency}"
+)
