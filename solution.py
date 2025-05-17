f36b186105c557374add2106c3a9ef5a7a5119a7
enissend <enissendikumana07@gmail.com>
2025-05-17 17:23:24 -0500

    imported requests library, created from_currency and to_currency variables

---
+++ solution.py
@@ -0,0 +1,9 @@
+import requests
+
+# currency user is converting from, upper() accepts only uppercase inputs
+from_currency = str(
+    input("Enter in the currency you'd like to convert from: ")).upper()
+
+# currency user is converting to, upper() accepts only uppercase inputs
+to_currency = str(
+    input("Enter in the currency you'd like to convert to: ")).upper()
