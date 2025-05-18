c62f73440a13153cfa359bdedef55ebee8a8fa43
enissend <enissendikumana07@gmail.com>
2025-05-17 23:05:39 -0500

    calculate and print converted amount using conversion rate from API response

--- solution.py
+++ solution.py
@@ -11,11 +11,17 @@ to_currency = str(
 amount = float(
     input("Enter in your monetary amount: ") )
  
- #fetches api, is successful when status is 200
+#fetches api, is successful when status is 200
 response = requests.get(
     f"https://v6.exchangerate-api.com/v6/9ddf48650227d232cc03556a/pair/{from_currency}/{to_currency}/{amount}")
 
+#extract conversion rate from api response
+conversion_rate = response.json()['conversion_rate']
 
+# calculates the converted amount
+converted_amount = amount * conversion_rate
+
+#prints the converted amount
 print(
-    f"{amount} {from_currency} is equal to {response.json()['conversion_rate']} {to_currency}"
+    f"{amount} {from_currency} is equal to {converted_amount} {to_currency}"
 )
